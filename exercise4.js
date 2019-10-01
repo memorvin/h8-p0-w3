2e78a35b54b71acac69154a5b97968b123a9d4ac
memorvin <alvina.tjia@yahoo.com>
2019-10-01 18:15:22 +0700

    edit3 exercise4

--- exercise4.js
+++ exercise4.js
@@ -1,56 +1,56 @@
-var input = ["0001", "Roman Alamsyah", "Bandar Lampung", "21/05/1989", "Membaca"]; 
+var input = ['0001', 'Roman Alamsyah', 'Bandar Lampung', '21/05/1989', 'Membaca']; 
 
 function dataHandling2(input) {
 
-  input.splice(1,1, 'Roman Alamsyah Elsharawy');
+  input.splice(1, 1, 'Roman Alamsyah Elsharawy');
   input.splice(2, 1, 'Provinsi Bandar Lampung');
-  input.splice(4,1);
+  input.splice(4, 1);
   input.splice(4, 2, 'Pria', 'SMA Internasional Metro');
   console.log(input);
 
-  var ttl1 = input[3].split("/");
+  var ttl1 = input[3].split('/');
   switch (ttl1[1]) {
     case '01':
-      console.log("Januari");
+      console.log('Januari');
       break;
     case '02':
-      console.log("Februari");
+      console.log('Februari');
       break;
     case '03':
-      console.log("Maret");
+      console.log('Maret');
       break;
     case '04':
-      console.log("April");
+      console.log('April');
       break;
     case '05':
-      console.log("Mei");
+      console.log('Mei');
       break;
     case '06':
-      console.log("Juni");
+      console.log('Juni');
       break;
     case '07':
-      console.log("Juli");
+      console.log('Juli');
       break;
     case '08':
-      console.log("Agustus");
+      console.log('Agustus');
       break;
     case '09':
-      console.log("September");
+      console.log('September');
       break;
     case '10':
-      console.log("Oktober");
+      console.log('Oktober');
       break;
     case '11':
-      console.log("November");
+      console.log('November');
       break;
     case '12':
-      console.log("Desember");
+      console.log('Desember');
   }
 
   console.log(ttl1.sort(function(a, b){return b-a}));
   
-  var ttl2 = input[3].split("/");
-  console.log(ttl2.join("-"));
+  var ttl2 = input[3].split('/');
+  console.log(ttl2.join('-'));
   
   console.log(input[1].slice(0,15));
 }
