19e927e3dea3faf95155d5e0014033897c6f3edb
memorvin <alvina.tjia@yahoo.com>
2019-10-01 18:10:02 +0700

    edit2 exercise4

--- exercise4.js
+++ exercise4.js
@@ -1,6 +1,11 @@
+var input = ["0001", "Roman Alamsyah", "Bandar Lampung", "21/05/1989", "Membaca"]; 
+
 function dataHandling2(input) {
-  
-  input.splice(1, 4, "Roman Alamsyah Elsharawy", "Provinsi Bandar Lampung", "21/05/1989", "Pria", "SMA Internasional Metro");
+
+  input.splice(1,1, 'Roman Alamsyah Elsharawy');
+  input.splice(2, 1, 'Provinsi Bandar Lampung');
+  input.splice(4,1);
+  input.splice(4, 2, 'Pria', 'SMA Internasional Metro');
   console.log(input);
 
   var ttl1 = input[3].split("/");
@@ -50,4 +55,4 @@ function dataHandling2(input) {
   console.log(input[1].slice(0,15));
 }
 
-dataHandling2(["0001", "Roman Alamsyah ", "Bandar Lampung", "21/05/1989", "Membaca"]);
+dataHandling2(input);
