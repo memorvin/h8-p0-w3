a47675dd9da8c3f817689a95a666e1c91a8b28b8
memorvin <alvina.tjia@yahoo.com>
2019-10-01 15:53:47 +0700

    edit exercise4

--- exercise4.js
+++ exercise4.js
@@ -4,44 +4,43 @@ function dataHandling2(input) {
   console.log(input);
 
   var ttl1 = input[3].split("/");
-  switch (Number(ttl1[1])) {
-    case 01:
-      return "Januari";
+  switch (ttl1[1]) {
+    case '01':
+      console.log("Januari");
       break;
-    case 02:
-      return "Februari";
+    case '02':
+      console.log("Februari");
       break;
-    case 03:
-      return "Maret";
+    case '03':
+      console.log("Maret");
       break;
-    case 04:
-      return "April";
+    case '04':
+      console.log("April");
       break;
-    case 05:
-      return "Mei";
+    case '05':
+      console.log("Mei");
       break;
-    case 06:
-      return "Juni";
+    case '06':
+      console.log("Juni");
       break;
-    case 07:
-      return "Juli";
+    case '07':
+      console.log("Juli");
       break;
-    case 08:
-      return "Agustus";
+    case '08':
+      console.log("Agustus");
       break;
-    case 09:
-      return "September";
+    case '09':
+      console.log("September");
       break;
-    case 10:
-      return "Oktober";
+    case '10':
+      console.log("Oktober");
       break;
-    case 11:
-      return "November";
+    case '11':
+      console.log("November");
       break;
-    case 12:
-      return "Desember";
+    case '12':
+      console.log("Desember");
   }
-  console.log(Number(ttl1[1]));
 
   console.log(ttl1.sort(function(a, b){return b-a}));
   
