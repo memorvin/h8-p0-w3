2f0f2f7be8a2882fec47a7dedb23e18fd0b190c5
memorvin <alvina.tjia@yahoo.com>
2019-10-01 11:33:26 +0700

    livecode1

---
+++ 1.js
@@ -0,0 +1,31 @@
+/*
+==============
+Bola ping-pong
+==============
+
+Di dalam sebuah kotak A ada 50 bola ping-pong.
+Setiap bola ping pong bisa berwarna merah atau putih.
+
+Buatlah pseudocode untuk menghitung jumlah
+bola ping-pong berwarna merah dan putih masing-masing di dalam kotak A.
+
+===== jawaban =====
+
+START
+SET bola = 50
+SET counter = 1
+SET merah = 0
+SET putih = 0
+WHILE counter < bola DO
+    IF bola = merah
+        merah = merah + 1
+    ELSE
+        putih = putih + 1
+    ENDIF
+    bola = bola - 1
+PRINT merah
+PRINT putih
+ENDWHILE
+END
+
+*/
